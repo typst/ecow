fee4c9160f465973d672334549027672e3a4d511
Lovecraftian Horror <LovecraftianHorror@pm.me>
2023-02-25 23:08:32 -0700

    Collapse loom test structure

--- tests/loom/mod.rs
+++
@@ -1,4 +0,0 @@
-mod vec;
-
-#[cfg(debug_assertions)]
-compile_error!("Loom tests are typically slow in debug mode. Run them with `--release`");
--- tests/stub.rs
+++
@@ -1,6 +0,0 @@
-#[cfg(loom)]
-/// Run loom tests with something like
-/// ```
-/// $ RUSTFLAGS="--cfg loom" cargo test --release loom
-/// ```
-mod loom;
